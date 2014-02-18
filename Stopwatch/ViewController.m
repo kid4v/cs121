d9a2d2d911a4620e9ea85a4f9a45a8bf4db316fe
Kevin Byrne <=>
2014-02-17 23:32:06 -0800

    Added stopwatch functionality to milliseconds with NSDate.

--- Stopwatch/ViewController.m
+++ Stopwatch/ViewController.m
@@ -7,17 +7,45 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/CAAnimation.h>
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+- (IBAction)buttonPressed:(id)sender;
+
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    
+    bool start;
+    
+    NSTimeInterval time;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.display.text = @"0:00.000";
+    start = false;
+
+}
+
+- (void) update {
+    if (start == false) {
+        return;
+    }
+    NSTimeInterval currentTime = [NSDate timeIntervalSinceReferenceDate];
+    NSTimeInterval elapsedTime = (currentTime - time);
+    
+    // 121.333 seconds
+    int minutes = (int)(elapsedTime / 60);
+    int seconds = (int)(elapsedTime - (minutes * 60));
+    int millis = (int)((elapsedTime - (minutes*60) - seconds) * 1000);
+    self.display.text = [NSString stringWithFormat:@"%u:%02u.%03u", minutes, seconds, millis];
+    
+    [self performSelector:@selector(update) withObject:self afterDelay:0.1];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +54,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    
+    if (start == false) {
+        start = true;
+        time = [NSDate timeIntervalSinceReferenceDate];
+        [sender setTitle:@"Stop!" forState:UIControlStateNormal];
+   
+        [self update];
+    } else {
+        start = false;
+        [sender setTitle:@"Start" forState:UIControlStateNormal];
+    }
+}
 @end
