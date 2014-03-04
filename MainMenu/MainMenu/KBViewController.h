f0983852fd52d4d2e13f763f7825bc7ade1bf25d
Kevin Byrne <=>
2014-03-04 10:38:33 -0800

    Adding mainmenu!

---
+++ MainMenu/MainMenu/KBViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBViewController.h
+//  MainMenu
+//
+//  Created by Kevin Byrne on 2/27/14.
+//  Copyright (c) 2014 Kevin Byrne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBViewController : UIViewController
+
+@end
---
+++ MainMenu/MainMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MainMenu
+//
+//  Created by Kevin Byrne on 2/27/14.
+//  Copyright (c) 2014 Kevin Byrne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBAppDelegate class]));
+    }
+}
