a9315880e7248fab13a95cfde2f9e08b8aa42bcc
CS121 <cs121@jarthurcss-MacBook-Pro-6.local>
2014-03-04 10:49:13 -0800

    added xyzViewcontroller

---
+++ ctest/ctest/XYZViewController.m
@@ -0,0 +1,29 @@
+//
+//  XYZViewController.m
+//  ctest
+//
+//  Created by CS121 on 2/19/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import "XYZViewController.h"
+
+@interface XYZViewController ()
+
+@end
+
+@implementation XYZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
