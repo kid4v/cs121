6e659a0f23a25da3baebb67c1802336468a707c2
billym <Billy@billys-air.home>
2014-04-14 11:32:37 -0700

    Started minigame3

---
+++ ctest/XYZMini2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZMini2ViewController.h
+//  ctest
+//
+//  Created by Billy Mills on 4/14/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZMini2ViewController : UIViewController
+
+@end
